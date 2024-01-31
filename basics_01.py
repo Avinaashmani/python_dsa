1c390f225a2bf823df81401912bf24e001b7f9d6
avinaash <avinaash.mani@gmail.com>
2024-01-31 15:01:37 +0530

    basics_01.py

---
+++ basics_01.py
@@ -0,0 +1,9 @@
+## Practice for Exceptions, Functions, Lists, Tuples, Dictionaries & Sets ##
+string_data = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+## Lists ##
+
+list_ex = list(string_data)
+print("LIST: ",list_ex)
+## Sets ##
+set_ex = set(string_data)
+print("SET: ",set_ex)
